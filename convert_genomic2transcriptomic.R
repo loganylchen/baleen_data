caac824c3e5c978fc017dd3ad8ed1fda341d433c
Yuelong Chen <yuelong.chen.btr@gmail.com>
2023-12-21 15:40:50 +0800

    initial commit

--- convert_genomic2transcriptomic.R
+++ convert_genomic2transcriptomic.R
@@ -2,7 +2,7 @@ library(ensembldb)
 gtf<-'Homo_sapiens.GRCh38.104.gtf'
 DB <- ensDbFromGtf(gtf=gtf)
 EDB<-EnsDb(DB)
-df <- read.csv('Hek293T_m6aceSeq_results.csv')
+df <- read.csv('hek293t_m6ace/Hek293T_m6aceSeq_results.csv')
 grange_range <- makeGRangesFromDataFrame(df,seqnames.field=c('Chr'),start.field='Start',end.field='End',strand.field='Strand')
 transcript_loc <- genomeToTranscript(grange_range,EDB)
 write.csv(as.data.frame(transcript_loc),'hek293t_m6ace/Hek293T_m6aceSeq_results_annotated.csv',quote=FALSE)
---
+++ get_final_benchmark_hek293t.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+print("Loading data...")
+hek293t_m6ace_df = pd.read_csv('hek293t_m6ace/Hek293T_m6aceSeq_results_annotated.csv', index_col=0)
+print("Drop the items that without tx_id. (Sorted by seq_name and seq_start)")
+hek293t_m6ace_df = hek293t_m6ace_df.dropna(subset=['tx_id']).sort_values(['seq_name', 'seq_start'])
+
+print("Loading data...")
+hek293t_m6ace_raw_df = pd.read_csv('hek293t_m6ace/Hek293T_m6aceSeq_results.csv').sort_values(['Chr', 'Start'])
+
+print("Merging annotated data and raw data with m6a results")
+merged_data = pd.merge(hek293t_m6ace_df, hek293t_m6ace_raw_df, left_on=['seq_name', 'seq_start'],
+                       right_on=['Chr', 'Start'], how='left')
+merged_data.to_csv('hek293t_m6ace/Hek293T_m6aceSeq_annotated_m6aceincluded.csv', sep='\t', index=False)
+
+print("get the bed region!!! expand 6bp upstream and 7bp downstream")
+
+merged_data['start'] = merged_data['start'] - 6
+merged_data['end'] = merged_data['end'] + 7
+merged_data.loc[merged_data['start'] < 0, 'start'] = 0
+
+merged_data.sort_values(['tx_id', 'start', 'end']).loc[:, ['tx_id', 'start', 'end'
+                                                           ]].to_csv(
+    'hek293t_m6ace/Hek293T_m6aceSeq_annotated_m6aceincluded.bed', sep='\t', index=False, header=False)
