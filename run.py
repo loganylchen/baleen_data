06197cd4905191d1b58fcd0fe50c8d46228b7427
loganylchen <yuelong.chen.btr@gmail.com>
2024-01-12 13:49:26 +0800

    multi-run

--- run.py
+++ run.py
@@ -39,6 +39,7 @@ def main():
     files = glob.glob('hek293t_m6ace/tmp/tmp_*.csv')
     df = pd.concat([pd.read_csv(f) for f in files])
     df.to_csv('hek293t_m6ace/Hek293T_m6aceSeq_results_annotated.csv', index=False)
+    run_command('python get_final_benchmark_hek293t.py')
 
 if __name__ == '__main__':
     main()
