485268f30114e32f03f8886bd75da0c0addd8b7f
loganylchen <yuelong.chen.btr@gmail.com>
2024-02-26 14:33:20 +0800

    multi-run

--- run_mES_MeRIP.py
+++ run_mES_MeRIP.py
@@ -27,20 +27,20 @@ def print_format_log(logstring, log_type='OUTPUT'):
 
 def main():
     run_command('Rscript get_ensembdb_mES.R')
-    df = pd.read_csv('hek293t_m6ace/m6ace_hek293t_GT_extend.csv')
-    contigs = df['Chr'].unique()
-    commands = [f'Rscript convert_genomic2transcriptomic.R {contig}' for contig in contigs]
-    os.makedirs('hek293t_m6ace/tmp', exist_ok=True)
+    df = pd.read_csv('good_region.bed',sep='\t',header=None)
+    contigs = df[0].unique()
+    commands = [f'Rscript convert_genomic2transcriptomic_mES.R {contig}' for contig in contigs]
+    os.makedirs('mES_MeRIP/tmp/', exist_ok=True)
     with Pool(50) as pool:
         pool.map(run_command, commands)
         pool.close()
         pool.join()
 
-    files = glob.glob('hek293t_m6ace/tmp/tmp_*.csv')
+    files = glob.glob('mES_MeRIP/tmp/tmp_*.csv')
     df = pd.concat([pd.read_csv(f) for f in files])
-    df.to_csv('hek293t_m6ace/Hek293T_m6aceSeq_results_annotated.csv', index=False)
-    run_command('python get_final_benchmark_hek293t.py')
-    run_command('bedtools merge -i hek293t_m6ace/Hek293T_m6aceSeq_annotated_m6aceincluded.bed > hek293t_m6ace/Hek293T.bed')
+    df.to_csv('mES_MeRIP/annotated.csv', index=False)
+    # run_command('python get_final_benchmark_hek293t.py')
+    # run_command('bedtools merge -i hek293t_m6ace/Hek293T_m6aceSeq_annotated_m6aceincluded.bed > hek293t_m6ace/Hek293T.bed')
 
 if __name__ == '__main__':
     main()
